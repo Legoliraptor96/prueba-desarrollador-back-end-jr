623286413383e6657551dbc50758d8a187d501f2
Legoliraptor96 <104172725+Legoliraptor96@users.noreply.github.com>
2022-04-26 20:11:16 -0500

    Prueba 1 terminada
    Prueba 1 terminada

--- prueba-desarrollador-backend-jr.py
+++ prueba-desarrollador-backend-jr.py
@@ -6,6 +6,15 @@ Alonso Daniel Jacobo Hernández
 import pandas as pd
 import xlrd
 
+
+
+#ignorar SettingWithCopyWarning 
+pd.options.mode.chained_assignment = None  # default='warn'
+
+
+
+#-----------------------Inicio Punto 2 --------------------------------------------------
+
 #Ruta donde se encuentra el archivo
 path=r"C:\Users\Alonso\Documents\GitHub\prueba-desarrollador-back-end-jr\CPdescarga.xls"
 
@@ -18,11 +27,11 @@ df_sepomex=pd.concat(SEPOMEX,ignore_index=True)
 #Prueba imprimir tablas concatenadas
 #print(df_sepomex)
 
-df_estado=df_sepomex[['d_codigo','c_estado']]
+df_estado=df_sepomex[['d_codigo','d_estado']]
 
 #Prueba imprimir tabla de Codigo vs Estado
 df_estado.rename({'d_codigo': 'Codigo'}, axis=1, inplace=True)
-df_estado.rename({'c_estado': 'Estado'}, axis=1, inplace=True)
+df_estado.rename({'d_estado': 'Estado'}, axis=1, inplace=True)
 
 #Prueba imprimir tabla de Codigo vs Estado
 #print(df_estado)
@@ -33,41 +42,26 @@ df_municipio=df_sepomex[['d_codigo','D_mnpio']]
 df_municipio.rename({'d_codigo': 'Codigo'}, axis=1, inplace=True)
 df_municipio.rename({'D_mnpio': 'Municipio'}, axis=1, inplace=True)
 
-#Prueba imprimir tabla de Codigo vs Municpio
+#Prueba imprimir tabla de Codigo vs Ciudad
 #print(df_municipio)
 
 df_ciudad=df_sepomex[['d_codigo','d_ciudad']]
 
-#Prueba imprimir tabla de Codigo vs Municpio
+#Prueba imprimir tabla de Codigo vs Ciudad
 df_ciudad.rename({'d_codigo': 'Codigo'}, axis=1, inplace=True)
 df_ciudad.rename({'d_ciudad': 'Ciudad'}, axis=1, inplace=True)
 
-#Prueba imprimir tabla de Codigo vs Municpio
-#print(df_municipio)
+#Prueba imprimir tabla de Codigo vs Ciudad
+#print(df_ciudad)
 
+#union de las 3 tablas 
+df_tablas=[df_estado,df_municipio,df_ciudad]
+df_union=pd.concat(df_tablas)
 
+#Prueba imprimir concatenacion de tablas
+#print(df_union)
 
+#-----------------------Fin Punto 2 --------------------------------------------------
 
-'''Apertura del archivo SEPOMEX, en modo lectura para evitar modificaciones
-
-#SEPOMEX = open("CPdescarga.txt",mode="r")
-
- df = pd.read_csv('CPdescarga.txt', sep='|')
-
-#print(SEPOMEX.read())
-'''
-
-'''
-with open("CPdescarga.txt") as SEPOMEX:
-    data = f.readlines()[2:100000]
-print(data)
-'''
-'''
-with open("CPdescarga.txt") as SEPOMEX:
-    next(SEPOMEX)  # Descartar la primera línea
-    next(SEPOMEX)  # Descartar la Segundo línea
-    for linea in SEPOMEX:
-        print(linea)
-'''
 
 
